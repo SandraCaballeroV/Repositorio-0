2944f65d378de182045ac6d0287fc430280c3fdf
unknown <sandracaballero11@gmail.com>
2021-11-11 17:34:19 -0500

    arreglo para despliegue

--- controllers/usuarios/controller.js
+++ controllers/usuarios/controller.js
@@ -17,12 +17,10 @@ const consultarUsuario = async (id, callback) => {
 
 const consultarOCrearUsuario = async (req, callback) => {
   console.log('Estoy llegando a crear usuario');
-  // 6.1. obtener los datos del usuario desde el token
   const token = req.headers.authorization.split('Bearer ')[1];
   const user = jwt_decode(token)['http://localhost/userData'];
   console.log(user);
-  // 6.2. con el correo del usuario o con el id de auth0, verificar si el usuario ya esta en la bd o no
-  const baseDeDatos = getDB();
+  
   await baseDeDatos.collection('usuario').findOne({ email: user.email }, async (err, response) => {
     console.log('response consulta bd', response);
     if (response) {
