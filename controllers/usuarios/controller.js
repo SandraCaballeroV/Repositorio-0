482911dd5d59c177d6d57a7c7131e83d235c4cc7
unknown <sandracaballero11@gmail.com>
2021-11-11 18:36:21 -0500

    controller modificacion

--- controllers/usuarios/controller.js
+++ controllers/usuarios/controller.js
@@ -1,14 +1,17 @@
 import { ObjectId } from 'mongodb';
 import { getDB } from '../../db/db.js';
 import jwt_decode from 'jwt-decode';
+
 const queryAllUsers = async (callback) => {
   const baseDeDatos = getDB();
   console.log('query');
   await baseDeDatos.collection('usuario').find({}).limit(50).toArray(callback);
 };
+
 const crearUsuario = async (datosUsuario, callback) => {
   const baseDeDatos = getDB();
   await baseDeDatos.collection('usuario').insertOne(datosUsuario, callback);
+
 };
 const consultarUsuario = async (id, callback) => {
   const baseDeDatos = getDB();
@@ -20,6 +23,8 @@ const consultarOCrearUsuario = async (req, callback) => {
   const token = req.headers.authorization.split('Bearer ')[1];
   const user = jwt_decode(token)['http://localhost/userData'];
   console.log(user);
+
+  const baseDeDatos = getDB();
   
   await baseDeDatos.collection('usuario').findOne({ email: user.email }, async (err, response) => {
     console.log('response consulta bd', response);
